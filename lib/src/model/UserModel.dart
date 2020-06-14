0927f0d8c96ce7133b87cb7311411b013dc1ecca
Kelvin C Espelimbergo <kelvinespelimbergo@gmail.com>
2020-06-14 13:37:01 +0800

    version 1.1

--- lib/src/model/UserModel.dart
+++ lib/src/model/UserModel.dart
@@ -1,65 +1,41 @@
 // To parse this JSON data, do
 //
-//     final signupModel = signupModelFromJson(jsonString);
+//     final userModel = userModelFromJson(jsonString);
 
 import 'dart:convert';
 
-UserModel signupModelFromJson(String str) => UserModel.fromJson(json.decode(str));
+UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
 
-String signupModelToJson(UserModel data) => json.encode(data.toJson());
+String userModelToJson(UserModel data) => json.encode(data.toJson());
 
 class UserModel {
-  String username;
-  String firstname;
-  String lastname;
-  String middlename;
-  String password;
-  String mobileno;
-  String email;
-  String success;
-  String message;
-  String picture;
-  String fbToken;
-
   UserModel({
-    this.username,
-    this.firstname,
-    this.lastname,
-    this.middlename,
-    this.password,
-    this.mobileno,
     this.email,
-    this.success,
+    this.password,
     this.message,
-    this.picture,
-    this.fbToken
+    this.success,
+
   });
 
+  String email;
+  String password;
+  String message;
+  String success;
+
+
   factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
-    username: json["username"],
-    firstname: json["firstname"],
-    lastname: json["lastname"],
-    middlename: json["middlename"],
-    password: json["password"],
-    mobileno: json["mobileno"],
     email: json["email"],
-    success: json['success'],
-    message: json['message'],
-      picture: json['picture'],
-      fbToken: json['fbToken']
+    password: json["password"],
+    message: json["message"],
+    success: json["success"],
+
   );
 
   Map<String, dynamic> toJson() => {
-    "username": username,
-    "firstname": firstname,
-    "lastname": lastname,
-    "middlename": middlename,
-    "password": password,
-    "mobileno": mobileno,
     "email": email,
-    "success": success,
+    "password": password,
     "message": message,
-    "picture": picture,
-    "fbToken": fbToken
+    "success": success,
+
   };
 }
