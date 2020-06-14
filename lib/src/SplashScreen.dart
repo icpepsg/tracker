a26936ea3b9031ab5b4b886e6ba9638628df6791
Kelvin C Espelimbergo <kelvinespelimbergo@gmail.com>
2020-06-14 13:36:36 +0800

    version 1.1

--- lib/src/SplashScreen.dart
+++ lib/src/SplashScreen.dart
@@ -22,7 +22,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Timer(Duration(seconds: 5), () {
+    Timer(Duration(seconds: 2), () {
       printUser();
     });
 
